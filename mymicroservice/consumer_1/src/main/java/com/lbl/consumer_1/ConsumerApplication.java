aaf9a3d5930dcd943cb77577a08dedea7828d524
Administrator <Administrator@PC-20180329VNFG>
2018-05-09 14:28:38 +0800

    12342134

--- mymicroservice/consumer_1/src/main/java/com/lbl/consumer_1/ConsumerApplication.java
+++ mymicroservice/consumer_1/src/main/java/com/lbl/consumer_1/ConsumerApplication.java
@@ -3,18 +3,22 @@ package com.lbl.consumer_1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /***
  * 
  * @author linbaolu
- * 2018-05-08
+ * 2018-05-05
  *
  */
 @SpringBootApplication  
 @EnableDiscoveryClient  
 @EnableFeignClients  
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class ConsumerApplication extends SpringBootServletInitializer {  
   
     public static void main(String[] args) {  
